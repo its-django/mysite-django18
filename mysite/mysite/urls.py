362d440ce4ec97e4cf9242e400d3503e990ba81d
myyang <ymy1019@gmail.com>
2014-12-08 21:17:17 +0800

    Module_05 - 媽，我在這
    1. 新增網址
    2. 新增視圖

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from .views import here
+
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^here/$', here),
 ]
