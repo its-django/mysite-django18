27fb6ed37b742036d0fa0d8e8061172ed8b056ea
myyang <ymy1019@gmail.com>
2014-12-15 02:00:05 +0800

    Module_13 - 自定義的登入與首頁
    1. 自定義的使用者登入頁面
    2. 自定義登入後跳轉的首頁

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from .views import here, math, welcome
+from .views import here, math, welcome, login, index
 from restaurants.views import menu, list_restaurants, comment
 
 
 urlpatterns = [
+    url('^index/$', index),
+    url(r'^accounts/login/$', login),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^here/$', here),
     url(r'^(\d{1,2})/math/(\d{1,2})/$', math),
