1db5a58d00d7de3a553f40deeb733be5ef76e3a6
myyang <ymy1019@gmail.com>
2014-12-26 22:41:39 +0800

    Module_17 - 使用ListView
    1. 使用ListView呈現餐廳列表
    2. 在dispatch加上裝飾器限制登入請求

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 from django.contrib.auth.views import login, logout
+from django.views.generic.base import TemplateView
 
 from . import views
 import restaurants.views
@@ -13,13 +14,13 @@ urlpatterns = [
     url(r'^accounts/logout/$', logout),
     url(r'^admin/', include(admin.site.urls)),
 
-    url('^index/$', views.IndexView.as_view()),
+    url('^index/$', TemplateView.as_view(template_name='index.html')),
     url(r'^accounts/register/$', views.register),
     url(r'^here/$', views.HereView.as_view()),
     url(r'^(\d{1,2})/math/(\d{1,2})/$', views.math),
     url(r'^welcome/$', views.welcome),
 
     url(r'^menu/$', restaurants.views.menu),
-    url(r'^restaurants_list/$', restaurants.views.list_restaurants),
+    url(r'^restaurants_list/$', restaurants.views.RestaurantsView.as_view()),
     url(r'^comment/(\d{1,5})/$', restaurants.views.comment),
 ]
